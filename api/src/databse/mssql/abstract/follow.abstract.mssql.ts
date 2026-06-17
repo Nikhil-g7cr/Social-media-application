81c1ef5384faa84de54a8de256bc060433493c7b
Nikhil Kumar <nikhil.kumar@g7cr.com>
2026-06-17 15:32:01 +0530

    Add: follow dao and asbtract

---
+++ api/src/databse/mssql/abstract/follow.abstract.mssql.ts
@@ -0,0 +1,19 @@
+import { AppResponse } from 'src/shared/appresponse.shared';
+import { Follow } from '../models';
+
+export abstract class FollowAbstractSQLDao {
+  abstract create(data: Partial<Follow>): Promise<Follow>;
+  abstract findOne(
+    followerId: string,
+    followingId: string,
+  ): Promise<Follow | null>;
+  abstract delete(followerId: string, followingId: string): Promise<number>;
+  abstract getFollowers(userId: string): Promise<Follow[]>;
+  abstract getFollowing(userId: string): Promise<Follow[]>;
+  abstract countFollowers(userId: string): Promise<number>;
+  abstract countFollowing(userId: string): Promise<number>;
+  abstract isFollowing(
+    followerId: string,
+    followingId: string,
+  ): Promise<boolean>;
+}
---
+++ api/src/databse/mssql/dao/follow.dao.ts
@@ -0,0 +1,82 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Follow } from '../models/follow.model';
+import { MsSqlConstants } from '../connection/constant.mssql';
+import { FollowAbstractSQLDao } from '../abstract/follow.abstract.mssql';
+
+@Injectable()
+export class FollowDao implements FollowAbstractSQLDao {
+    constructor(
+        @Inject(MsSqlConstants.FOLLOW)
+        private readonly followModel: typeof Follow,
+    ) {}
+
+    async create(data: Partial<Follow>): Promise<Follow> {
+        return this.followModel.create(data as Follow);
+    }
+
+    async findOne(followerId: string, followingId: string): Promise<Follow | null> {
+        return this.followModel.findOne({
+            where: {
+                FollowerID: followerId,
+                FollowingID: followingId,
+            },
+        });
+    }
+
+    async delete(followerId: string, followingId: string): Promise<number> {
+        return this.followModel.destroy({
+            where: {
+                FollowerID: followerId,
+                FollowingID: followingId,
+            },
+        });
+    }
+
+    async getFollowers(userId: string): Promise<Follow[]> {
+        return this.followModel.findAll({
+            where: {
+                FollowingID: userId,
+            },
+            include: ['Follower'],
+        });
+    }
+
+    async getFollowing(userId: string): Promise<Follow[]> {
+        return this.followModel.findAll({
+            where: {
+                FollowerID: userId,
+            },
+            include: ['Following'],
+        });
+    }
+
+    async countFollowers(userId: string): Promise<number> {
+        return this.followModel.count({
+            where: {
+                FollowingID: userId,
+            },
+        });
+    }
+
+    async countFollowing(userId: string): Promise<number> {
+        return this.followModel.count({
+            where: {
+                FollowerID: userId,
+            },
+        });
+    }
+
+    async isFollowing(
+        followerId: string,
+        followingId: string,
+    ): Promise<boolean> {
+        const follow = await this.followModel.findOne({
+            where: {
+                FollowerID: followerId,
+                FollowingID: followingId,
+            },
+        });
+
+        return !!follow;
+    }
+}
