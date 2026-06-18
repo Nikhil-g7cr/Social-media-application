5ac2a4d5e0e05e6929997b23da84dfb0208c3a5c
Nikhil Kumar <nikhil.kumar@g7cr.com>
2026-06-18 12:41:16 +0530

    ADD: addinf the folder structure

---
+++ ui/src/shared/shared-components/AsyncAction.tsx
@@ -0,0 +1,9 @@
+const AsyncAction = ()=>{
+    return (
+        <div className='AsyncAction'> 
+            <h1>AsyncAction</h1>
+        </div>
+    )
+}
+ 
+export default AsyncAction;
---
+++ ui/src/shared/shared-components/DynamicForm.tsx
@@ -0,0 +1,9 @@
+const DynamicForm = ()=>{
+    return (
+        <div className='DynamicForm'> 
+            <h1>DynamicForm</h1>
+        </div>
+    )
+}
+ 
+export default DynamicForm;
