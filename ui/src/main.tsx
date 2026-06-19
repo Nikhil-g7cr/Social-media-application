152364f1ac55933dc3c3b0ec1b71829efc05ccdf
Nikhil Kumar <nikhil.kumar@g7cr.com>
2026-06-19 10:22:09 +0530

    ADD: import itailwind in index file

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,8 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 createRoot(document.getElementById("root")!).render(
   <Router>
+    <Provider>
+
     <App />
+    </Provider>
   </Router>,
 );
