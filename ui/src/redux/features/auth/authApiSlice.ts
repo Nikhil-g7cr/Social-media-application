9506f686cccced7c247aec2539ec7d6eb40936e5
Nikhil Kumar <nikhil.kumar@g7cr.com>
2026-07-10 19:04:09 +0530

    remove: the logout mutaion from the apislice.

--- ui/src/redux/features/auth/authApiSlice.ts
+++ ui/src/redux/features/auth/authApiSlice.ts
@@ -1,32 +1,22 @@
-import { apiSlice } from '../../apiSlice';
+import { apiSlice } from "../../apiSlice";
 
 export const authApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        signup: builder.mutation<any, any>({
-            query: (payload) => ({
-                url: '/auth/signup',
-                method: 'POST',
-                data: payload,
-            }),
-        }),
-        login: builder.mutation<any, any>({
-            query: (payload) => ({
-                url: '/auth/login',
-                method: 'POST',
-                data: payload,
-            }),
-        }),
-        logout: builder.mutation<any, void>({
-            query: () => ({
-                url: '/auth/logout',
-                method: 'POST',
-            }),
-        }),
+  endpoints: (builder) => ({
+    signup: builder.mutation<any, any>({
+      query: (payload) => ({
+        url: "/auth/signup",
+        method: "POST",
+        data: payload,
+      }),
     }),
+    login: builder.mutation<any, any>({
+      query: (payload) => ({
+        url: "/auth/login",
+        method: "POST",
+        data: payload,
+      }),
+    }),
+  }),
 });
 
-export const {
-    useSignupMutation,
-    useLoginMutation,
-    useLogoutMutation,
-} = authApiSlice;
+export const { useSignupMutation, useLoginMutation } = authApiSlice;
