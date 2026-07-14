93d018115fa027dd581f4b67ad29957ca9ee6ffe
Nikhil Kumar <nikhil.kumar@g7cr.com>
2026-07-14 18:13:15 +0530

    feat: added a scrollToTop component whihc will automatically scrolle dto the top when we open new page

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,8 +4,10 @@ import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store"
+import ScrollToTop from "./shared/shared-components/scrolledTop.tsx";
 createRoot(document.getElementById("root")!).render(
   <Router>
+    <ScrollToTop />
     <Provider store={store}>
       <App />
     </Provider>
